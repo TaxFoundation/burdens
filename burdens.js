ded281a0b64c3f35f8246dac8daa98384b8b7e35
Tom VanAntwerp <vanantwerp@gmail.com>
2016-01-19 16:11:38 -0500

    Add basic data parsing and filtering

--- burdens.js
+++ burdens.js
@@ -1,3 +1,23 @@
-d3.csv('burdens.csv', function(){
-  
-});
+var data = [];
+var id = 1;
+
+d3.csv('burdens.csv', function(d) {
+    return {
+      id: +d.id,
+      name: d.state,
+      year: d.year,
+      incomePerCapita: +d.incomePerCapita,
+      taxPaidToOwnState: +d.taxPaidToOwnState,
+      taxPaidToOtherState: +d.taxPaidToOtherState,
+      taxPaidTotal: +d.taxPaidToOwnState + +d.taxPaidToOtherState,
+      burdenRate: +d.burdenRate,
+    };
+  }, function(error, rows) {
+    if (error) { console.log(error); }
+
+    data = rows;
+  });
+
+function filterByState(row) {
+  return row.id === id;
+}
