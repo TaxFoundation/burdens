bd7e78f30f8740475915b3cec0385037d2d690a4
Tom VanAntwerp <vanantwerp@gmail.com>
2016-01-19 17:17:55 -0500

    Basic chart for one state all years works

--- burdens.js
+++ burdens.js
@@ -1,23 +1,57 @@
-var data = [];
-var id = 1;
+(function() {
+  'use strict';
 
-d3.csv('burdens.csv', function(d) {
-    return {
-      id: +d.id,
-      name: d.state,
-      year: d.year,
-      incomePerCapita: +d.incomePerCapita,
-      taxPaidToOwnState: +d.taxPaidToOwnState,
-      taxPaidToOtherState: +d.taxPaidToOtherState,
-      taxPaidTotal: +d.taxPaidToOwnState + +d.taxPaidToOtherState,
-      burdenRate: +d.burdenRate,
-    };
-  }, function(error, rows) {
-    if (error) { console.log(error); }
+  var burdens;
+  var DATA_PATH = 'burdens.csv';
+  var INITIAL_STATE = '1';
+  var INITIAL_YEAR = '2012';
 
-    data = rows;
+  $(function() {
+    queue()
+      .defer(d3.csv, DATA_PATH)
+      .await(function(error, data) {
+        if (error) { console.log(error); }
+
+        burdens.initialize(data);
+      });
   });
 
-function filterByState(row) {
-  return row.id === id;
-}
+  burdens = {
+    initialize: function(data) {
+      this.data = data;
+      this.drawTableState(this.filterByState(INITIAL_STATE));
+    },
+
+    filterByState: function(state) {
+      var state = state || INITIAL_STATE;
+      var stateData = burdens.data.filter(function(stateData) {
+        return stateData.id == state;
+      });
+
+      return stateData;
+    },
+
+    filterByYear: function(year) {
+      var year = year || INITIAL_YEAR;
+      var yearData = burdens.data.filter(function(yearData) {
+        return yearData.year == year;
+      });
+    },
+
+    drawTableState: function(data) {
+      d3.select('#burdens').selectAll('tr').remove();
+
+      var states = d3.select('#burdens').selectAll('tr');
+
+      states.data(data).enter().append('tr').html(function(d) {
+        return '<td>' + d.year + '</td>'
+        + '<td>' + d.burdenRate + '</td>'
+        + '<td>' + d.incomePerCapita + '</td>'
+        + '<td>' + d.taxPaidToOwnState + '</td>'
+        + '<td>' + d.taxPaidToOtherState + '</td>'
+        + '<td>' + (+d.taxPaidToOwnState + +d.taxPaidToOtherState) + '</td>';
+      });
+    },
+  };
+
+})();
