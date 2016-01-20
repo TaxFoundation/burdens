51a657ce27364ccc28cfccc5b9296ee5a6228bc2
Tom VanAntwerp <vanantwerp@gmail.com>
2016-01-19 19:30:49 -0500

    Real vs nominal selection works

--- burdens.js
+++ burdens.js
@@ -6,7 +6,45 @@
   var INITIAL_STATE = '1';
   var INITIAL_YEAR = '2012';
   var formatPercents = d3.format(',.1%');
-  var formatDollars = d3.format("$,.2f");
+  var formatDollars = d3.format('$,.2f');
+  var ADJUSTMENTS = {
+    1977: 3.789316804,
+    1978: 3.519759468,
+    1979: 3.163574057,
+    1980: 2.786713938,
+    1981: 2.524820117,
+    1982: 2.378972674,
+    1983: 2.304829088,
+    1984: 2.210739312,
+    1985: 2.134577902,
+    1986: 2.094278319,
+    1987: 2.020449471,
+    1988: 1.941182614,
+    1989: 1.852305413,
+    1990: 1.757181911,
+    1991: 1.685688725,
+    1992: 1.636357364,
+    1993: 1.589463832,
+    1994: 1.549011261,
+    1995: 1.506595838,
+    1996: 1.463632688,
+    1997: 1.430451331,
+    1998: 1.408480442,
+    1999: 1.378278557,
+    2000: 1.333386972,
+    2001: 1.296683635,
+    2002: 1.276468077,
+    2003: 1.247978588,
+    2004: 1.215554966,
+    2005: 1.175659829,
+    2006: 1.138865706,
+    2007: 1.107316626,
+    2008: 1.066377136,
+    2009: 1.070181963,
+    2010: 1.052913512,
+    2011: 1.020692531,
+    2012: 1,
+  };
 
   $(function() {
     queue()
@@ -25,9 +63,13 @@
 
       $('#state').change(function(e) {
         var state = $(this).val();
-        console.log(state);
         burdens.drawTableState(burdens.filterByState(state));
       });
+
+      $('input[name=dollars]').change(function() {
+        var state = $('#state').val();
+        burdens.drawTableState(burdens.filterByState(state));
+      })
     },
 
     filterByState: function(state) {
@@ -46,6 +88,14 @@
       });
     },
 
+    adjustDollars: function(value, year) {
+      if ($('input[name="dollars"]:checked').val() === 'real') {
+        return value * ADJUSTMENTS[year];
+      } else {
+        return value;
+      }
+    },
+
     drawTableState: function(data) {
       d3.select('#burdens').selectAll('tr').remove();
 
@@ -54,10 +104,10 @@
       states.data(data).enter().append('tr').html(function(d) {
         return '<td class="number">' + d.year + '</td>'
         + '<td class="number">' + formatPercents(d.burdenRate) + '</td>'
-        + '<td class="number">' + formatDollars(d.incomePerCapita) + '</td>'
-        + '<td class="number">' + formatDollars(d.taxPaidToOwnState) + '</td>'
-        + '<td class="number">' + formatDollars(d.taxPaidToOtherState) + '</td>'
-        + '<td class="number">' + formatDollars(+d.taxPaidToOwnState + +d.taxPaidToOtherState) + '</td>';
+        + '<td class="number">' + formatDollars(burdens.adjustDollars(d.incomePerCapita, +d.year)) + '</td>'
+        + '<td class="number">' + formatDollars(burdens.adjustDollars(d.taxPaidToOwnState, +d.year)) + '</td>'
+        + '<td class="number">' + formatDollars(burdens.adjustDollars(d.taxPaidToOtherState, +d.year)) + '</td>'
+        + '<td class="number">' + formatDollars(burdens.adjustDollars(+d.taxPaidToOwnState + +d.taxPaidToOtherState, +d.year)) + '</td>';
       });
     },
   };
